cccb5e544fe16273b72106ec9efd5317ea14f8c8
Ali Deeb <ali1alideeb@gmail.com>
2024-08-22 11:41:17 +0300

    added about me text.

--- lib/screens/widgets/about_me_widget.dart
+++ lib/screens/widgets/about_me_widget.dart
@@ -9,8 +9,10 @@ class AboutMeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final color = Theme.of(context).extension<CustomThemeColors>()?.textColor;
-    const aboutMe =
-        "I am a usability analyst,researcher and designer with 5+ years of experience in the field of user experience design (UX) and user-centered design (UCD).I help industries, companies, professionals and individuals to excel and boost their identity, service and applications in their digital phase.";
+    const aboutMe = '''
+I am an experienced and dedicated Flutter Developer with over a decade of professional experience in the tech industry, specializing in mobile application development.My technical expertise spans a comprehensive array of technologies including Android, Kotlin, Java, and cross-platform frameworks such as Flutter and Kotlin Native.Driven by a passion for developing innovative mobile applications, I thrive in environments where I can transform complex challenges into intuitive, scalable, and reliable products.
+My career is also marked by a commitment to continuous learning and adaptation, integrating the latest technologies into my work to stay at the forefront of industry trends. Whether working on sports, media, or health applications, I bring a focused approach to quality and an enthusiasm for seeing projects from conception to successful deployment.
+''';
 
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
